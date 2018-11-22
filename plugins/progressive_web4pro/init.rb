67942d6c956a5488ddc497df2037520feb15277a
alexandreiev <aandreiev@ergoserv.com>
2018-11-22 17:34:05 +0200

    Add progressive web4pro customs plugin

---
+++ plugins/progressive_web4pro/init.rb
@@ -0,0 +1,16 @@
+require 'redmine'
+
+Redmine::Plugin.register :progressive_web4pro do
+  name 'Progressive Web4pro Customs'
+  author 'Alex Andreiev @ ErgoServ'
+  author_url 'https://www.ergoserv.com'
+  description 'Pack of extensions for Web4pro'
+  version '1.0.0'
+  requires_redmine :version_or_higher => '3.2'
+  settings :default => {
+    'status_progressive_web4pro' => false,
+    'estimated_developer_time_field' => ''
+  }, :partial => 'settings/progressive_web4pro_settings'
+end
+
+require 'settings_helper_patch'
